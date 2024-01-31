6d98ccdd9fb6dd80c169b33562b48ffbc4e9eaa7
kvit23 <elsagheer9@gmail.com>
2024-02-01 01:21:15 +0200

    models update

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
@@ -16,19 +17,42 @@ class Book extends Model
     }
 
 
-    public function scopeTitle(Builder $query, string $title) : Builder
+    public function scopeTitle(Builder $query, string $title) : Builder | QueryBuilder
     {
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 
-    public function scopePopular(Builder $query) : Builder
+    public function scopePopular(Builder $query, $from = null, $to = null) : Builder | QueryBuilder
     {
-        return $query->withCount('reviews')->orderBy('reviews_count', 'desc');
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])
+            ->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRated(Builder $query) : Builder
+    public function scopeHighestRated(Builder $query) : Builder | QueryBuilder
     {
-        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
     }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    private function dateRangeFilter(Builder $query, $from= null , $to = null)
+    {
+        if($from && !$to){
+            $query->where('created_at', '>=', $from);
+        }else if (!$from && $to){
+            $query->where('created_at', '<=', $to);
+        }else if ($from && $to){
+            $query->whereBetween('created_at' ,[$from, $to]);
+        }
+    }
+
 
 }
