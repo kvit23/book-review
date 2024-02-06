3637095133263b2bf35de77810a5b6936f5d72bc
kvit23 <elsagheer9@gmail.com>
2024-02-06 11:02:17 +0200

    finished

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
+use App\Models\Book;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +18,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+
+    return redirect()->route('books.index');
 });
+
+// Route::get('/books', function (){
+//     return Book::paginate(15);
+// });
+
+Route::resource('books', BookController::class)
+    ->only(['index', 'show']);
+
+Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only(['create', 'store']);
